4617d78c852b539344d692614af1536043b25888
Marco <marco_rg@msn.com>
2025-02-06 21:24:40 -0600

    Variables

---
+++ 01Variables/js/script.js
@@ -0,0 +1,27 @@
+// //Variable String
+//  var producto = "Monitor";
+// console.log(producto);
+
+// //Variable Int
+// producto = 20 ; 
+// console.log(producto);
+
+// //Multiples Variables
+
+// var categoria = "Computadoras",
+//     marca = "Lenovo",
+//     calificacion = "10";
+
+// Nueva versión
+
+let producto = "Producto Let";
+console.log(producto);
+
+    producto ="Monitor";
+    console.log(producto);
+
+    producto = true;
+    console.log(producto);
+
+    producto = null;
+    console.log(producto);
